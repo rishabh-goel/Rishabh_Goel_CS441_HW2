2628d5c67ea89bd6746a2ecdce2b24ee7d2dce40
rishabhgoel <goel.rishabh12@gmaill.com>
2021-10-18 18:09:05 -0500

    Comparator Class for Descending Sort

---
+++ src/main/scala/MapReduce/ValueComparator.scala
@@ -0,0 +1,15 @@
+package MapReduce
+
+import org.apache.hadoop.io.IntWritable.Comparator
+import org.apache.hadoop.io.{IntWritable, Text, WritableComparable, WritableComparator}
+
+import scala.language.postfixOps
+class ValueComparator extends WritableComparator(classOf[Text], true){
+
+  override def compare(a: WritableComparable[_], b: WritableComparable[_]): Int = {
+    val key1 = a.toString.toInt
+    val key2 = b.toString.toInt
+
+    -1*key1.compareTo(key2)
+  }
+}
